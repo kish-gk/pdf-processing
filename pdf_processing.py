c26f20aa3f3dda49cfae1176b8149c8967c837ba
Kishore <61354468+gkrockz@users.noreply.github.com>
2021-10-19 14:36:29 +0530

    updated script

--- pdf_processing.py
+++ pdf_processing.py
@@ -2,8 +2,8 @@ import PyPDF2
 
 with open('file1.pdf', 'rb') as file:
 	reader_obj = PyPDF2.PdfFileReader(file)
-	page = reader.getPage(0)
-	print(f"Total no.of pages: {reader.numPages}")
+	page = reader_obj.getPage(0)
+	print(f"Total no.of pages: {reader_obj.numPages}")
 	page.rotateClockwise(180)
 
 	writer_obj = PyPDF2.PdfFileWriter()
@@ -11,11 +11,15 @@ with open('file1.pdf', 'rb') as file:
 	with open('rotated_file.pdf', 'wb') as out_file:
 		writer_obj.write(out_file)
 
+	password = "pass123"
+	writer_obj.encrypt(password)
+
 """
 	1. getPage(index) - fetch page at index.
 	2. rotateClockwise(deg) - Rotates PDF by specified angle.
 	3. numPages - total no.of pages in PDF 
 	4. addPage - Adds the specified page to the writer_obj 
 	5. write - writes the specified content into the file.
+	6. encrypt - Encrypts the document with the specified password
 """
 
